fa5cb7e835e509b38ecab97d2afa59c7b2a53260
atishay-nar <atishayna@gmail.com>
2025-12-08 15:05:29 -0500

    added distribution drift feature, with corresponding plot feature.

--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import numpy as np
 from bayesian_observer import BiasedBayesianObserver
 import matplotlib.pyplot as plt
 
+
 # helper function to clamp probabilities between 0 and 1 in drift model
 def clamp(x):
     if x < 0.0:
@@ -10,36 +11,46 @@ def clamp(x):
         return 1.0
     return x
 
+
 # inputs the ground truth probability of heads, prior, confirmation multiplier, and number of trials
 # outputs a series of posteriors over trials as a np.array
-def sim(ground_truth, prior, confirmation_multiplier, n_trials=100, drift=0):
+def sim(ground_truth, prior, confirmation_multiplier, n_trials=100, drift=0.0):
     agent = BiasedBayesianObserver(prior, confirmation_multiplier)
     posterior = []
+    truth_series = []
     for _ in range(n_trials):
         prediction = agent.prediction()
         evidence = np.random.binomial(n=1, p=ground_truth)
         agent.observe(evidence, prediction)
         posterior.append(agent.get_posterior())
-    return np.array(posterior)
+
+        # optional drift in ground truth
+        truth_series.append(ground_truth)
+        if drift != 0.0:
+            ground_truth = clamp(ground_truth + np.random.normal(0, drift))
+    return np.array(posterior), np.array(truth_series)
 
 
 if __name__ == "__main__":
     # configuration
-    ground_truth = 0.9
+    ground_truth = 0.5
     prior = np.array([0, 0])
-    confirmation_multiplier = 2
-    n_trials = 100
+    confirmation_multiplier = 1
+    n_trials = 1000
+    drift = 0.0
 
     # run simulation
-    series = sim(ground_truth, prior, confirmation_multiplier, n_trials)
-    error = np.abs(series - ground_truth)
-    # plot
+    post_series, truth_series = sim(
+        ground_truth, prior, confirmation_multiplier, n_trials, drift
+    )
+    error = np.abs(post_series - truth_series)
 
+    # plot
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
 
-    ax1.plot(series, color="b")
+    ax1.plot(post_series, color="b")
+    ax1.plot(truth_series, color="g", linestyle="dashed")
     ax1.set_title("Posterior over Trials")
-    ax1.hlines(ground_truth, 0, n_trials, colors="g", linestyles="dashed")
 
     ax2.plot(error, color="r")
     ax2.set_title("Error over Trials")
