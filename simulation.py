7bcfe8b0f5899472cdb90ce0530d5303521e13ae
atishay-nar <atishayna@gmail.com>
2025-12-08 13:15:46 -0500

    added error tracking and a plot to visualize distribution convergence.

--- simulation.py
+++ simulation.py
@@ -1,24 +1,30 @@
 import numpy as np
 from bayesian_observer import BiasedBayesianObserver
+import matplotlib.pyplot as plt
 # inputs the ground truth probability of heads, prior, confirmation multiplier, and number of trials
 # outputs a series of posteriors over trials as a np.array
 def sim(ground_truth, prior, confirmation_multiplier, n_trials=100):
     agent = BiasedBayesianObserver(prior, confirmation_multiplier)
     posterior = []
+    post_error = []
     for _ in range(n_trials):
         prediction = agent.prediction()
         evidence = np.random.binomial(n=1, p=ground_truth)
         agent.observe(evidence, prediction)
         posterior.append(agent.get_posterior())
-    return np.array(posterior)
+        post_error.append(abs(ground_truth - agent.get_posterior()))
+    return np.array(posterior), np.array(post_error)    
 
 if __name__ == "__main__":
     # configuration
     ground_truth = 0.5
     prior= np.array([0,0])
-    confirmation_multiplier = 2
-    n_trials = 100
+    confirmation_multiplier = 1
+    n_trials = 1000
 
     # run simulation
-    series = sim(ground_truth, prior, confirmation_multiplier, n_trials)
+    series, error = sim(ground_truth, prior, confirmation_multiplier, n_trials)
     print(f"final result: posterior after {n_trials} trials: {series[-1]:.2f}")
+    plt.plot(series)
+    plt.plot(error)
+    plt.show()
